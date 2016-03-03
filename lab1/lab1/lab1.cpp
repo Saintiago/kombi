c375912126c7e5ea1844767c741434c903834e1e
Saintiago <techpriest2007@yandex.ru>
2016-03-03 22:58:14 +0300

    Исправил входные данные.

--- lab1/lab1/lab1.cpp
+++ lab1/lab1/lab1.cpp
@@ -10,21 +10,21 @@ int main()
 {
 	cout << "Spawning 10 permutations..." << endl;
 	clock_t startTime = clock();
-	long permutationsCount = CPermutation(3).SpawnPermutations(false);
+	long permutationsCount = CPermutation(10).SpawnPermutations(false);
 	double duration = (std::clock() - startTime) / (double)CLOCKS_PER_SEC;
 	cout << "Permutations count: " << permutationsCount << endl;
 	cout << "Time spent: " << duration << " seconds" << endl << endl;
 
 	cout << "Spawning 20 permutations..." << endl;
 	startTime = clock();
-	permutationsCount = CPermutation(5).SpawnPermutations(false);
+	permutationsCount = CPermutation(20).SpawnPermutations(false);
 	duration = (std::clock() - startTime) / (double)CLOCKS_PER_SEC;
 	cout << "Permutations count: " << permutationsCount << endl;
 	cout << "Time spent: " << duration << " seconds" << endl << endl;
 
 	cout << "Spawning 25 permutations..." << endl;
 	startTime = clock();
-	permutationsCount = CPermutation(10).SpawnPermutations(false);
+	permutationsCount = CPermutation(25).SpawnPermutations(false);
 	duration = (std::clock() - startTime) / (double)CLOCKS_PER_SEC;
 	cout << "Permutations count: " << permutationsCount << endl;
 	cout << "Time spent: " << duration << " seconds" << endl;
