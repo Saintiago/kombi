d4713d281bd67a9eae506150a5efabf65b749ac2
Saintiago <techpriest2007@yandex.ru>
2016-03-06 18:10:26 +0300

    Начал делать 3 задание первой лабы.

---
+++ lab1/ex3/Sequence.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "Sequence.h"
+
+
+CSequence::CSequence(int n, int k)
+	: m_n(n)
+	, m_k(k)
+{
+}
+
+
+
+CSequence::~CSequence()
+{
+}
---
+++ lab1/ex3/Sequence.h
@@ -0,0 +1,11 @@
+#pragma once
+class CSequence
+{
+public:
+	CSequence(int n, int k);
+	~CSequence();
+
+private:
+	int m_n, m_k;
+};
+
