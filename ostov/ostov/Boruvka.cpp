cd63f8fb0219573086e6fec922a560c0c91e1b39
Saintiago <techpriest2007@yandex.ru>
2016-06-22 02:20:49 +0300

    Продолжение 3й лабы

--- ostov/ostov/Boruvka.cpp
+++ ostov/ostov/Boruvka.cpp
@@ -14,7 +14,6 @@ CBoruvka::~CBoruvka()
 
 void CBoruvka::MakeGraph(ifstream & fin)
 {
-	//m_graph.CreateByAdjacencyMatrix(fin);
 	m_graph.CreateByArcList(fin);
 }
 
@@ -23,7 +22,7 @@ CGraph CBoruvka::GetGraph()
 	return m_graph;
 }
 
-CComponent CBoruvka::GetOstov()
+ComponentPtr CBoruvka::GetOstov()
 {
 	InitComponents(m_graph.GetNodes());
 
@@ -31,9 +30,12 @@ CComponent CBoruvka::GetOstov()
 	{
 		for (auto const &component : m_components)
 		{
-			component->AddClosestNode();
+			AddClosestNode(component);
 		}
+		resetComponents();
 	}
+
+	return m_components.at(0);
 }
 
 void CBoruvka::InitComponents(NodePtrVector nodes)
@@ -42,18 +44,94 @@ void CBoruvka::InitComponents(NodePtrVector nodes)
 	{
 		m_components.push_back(make_shared<CComponent>(node));
 	}
-
-	for (auto const &component : m_components)
-	{
-		AddClosestNode(component);
-	}
 }
 
 void CBoruvka::AddClosestNode(ComponentPtr component)
 {
+	NodePathNodeVector allLinks = {};
+
 	for (auto const &node : component->GetNodes())
 	{
+		NodePathNodeVector nodeLinks = node->GetNodeLinks();
+		allLinks.insert(allLinks.end(), nodeLinks.begin(), nodeLinks.end());
+	}
 
+	NodePathNodeVector freeLinks = {};
+
+	for (auto const &link : allLinks)
+	{
+		if (!component->hasNode(link.second.first))
+		{
+			freeLinks.push_back(link);
+		}
+	}
+
+	NodePathNode closestLink = GetClosestLink(freeLinks);
+	component->AddNode(m_graph.GetNode(closestLink.second.first));
+	AddLink(closestLink);
+}
+
+NodePathNode CBoruvka::GetClosestLink(NodePathNodeVector links)
+{
+	NodePathNode closestLink = links.at(0);
+
+	for (auto const &link : links)
+	{
+		if (link.second.second < closestLink.second.second)
+		{
+			closestLink = link;
+		}
+	}
+	return closestLink;
+}
+
+void CBoruvka::resetComponents()
+{
+	NodePtrVector allNodes = m_graph.GetNodes();
+	while (allNodes.size() > 0)
+	{
+		m_components.push_back(GetNewComponent(allNodes));
 	}
 }
 
+void CBoruvka::AddLink(NodePathNode link)
+{
+	m_links.push_back(link);
+}
+
+ComponentPtr CBoruvka::GetNewComponent(NodePtrVector & allNodes)
+{
+	ComponentPtr comp = make_shared<CComponent>(allNodes.at(0));
+
+	ExpandComponent(allNodes.at(0), comp, allNodes);
+
+	allNodes.erase(allNodes.begin());
+}
+
+void CBoruvka::ExpandComponent(NodePtr node, ComponentPtr & comp, NodePtrVector & allNodes)
+{
+	comp->AddNode(node);
+	for (auto &adjacentNode : GetAdjacentNodes(node))
+	{
+		ExpandComponent(adjacentNode, comp, allNodes);
+	}
+	allNodes.erase(allNodes.begin() + node->GetIndex());
+}
+
+NodePtrVector CBoruvka::GetAdjacentNodes(NodePtr node)
+{
+	NodePtrVector adjacentNodes = {};
+	unsigned index = node->GetIndex();
+	for (auto const &link : m_links)
+	{
+		if (link.first == index)
+		{
+			adjacentNodes.push_back(m_graph.GetNode(link.second.first));
+		}
+		if (link.second.first == index)
+		{
+			adjacentNodes.push_back(m_graph.GetNode(link.first));
+		}
+	}
+	return adjacentNodes;
+}
--- ostov/ostov/Boruvka.h
+++ ostov/ostov/Boruvka.h
@@ -12,13 +12,21 @@ public:
 	void MakeGraph(std::ifstream & fin);
 	CGraph GetGraph();
 
-	CComponent GetOstov();
+	ComponentPtr GetOstov();
 	
 
 private:
 	CGraph m_graph;
 	ComponentPtrVector m_components = {};
+	NodePathNodeVector m_links = {};
 
 	void InitComponents(NodePtrVector nodes);
 	void AddClosestNode(ComponentPtr component);
+	NodePathNode GetClosestLink(NodePathNodeVector links);
+	void resetComponents();
+	void AddLink(NodePathNode link);
+
+	ComponentPtr GetNewComponent(NodePtrVector & allNodes);
+	void ExpandComponent(NodePtr node, ComponentPtr & comp, NodePtrVector & allNodes);
+	NodePtrVector GetAdjacentNodes(NodePtr node);
 };
