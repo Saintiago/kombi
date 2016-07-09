a0451e19e8df89520b0decd0a3600b67e75cd826
Saintiago <techpriest2007@yandex.ru>
2016-07-10 00:11:45 +0300

    Алгоритм Форда-Фалкерсона.

---
+++ falkerson/falkerson/ReadMe.txt
@@ -0,0 +1,40 @@
+========================================================================
+    CONSOLE APPLICATION : falkerson Project Overview
+========================================================================
+
+AppWizard has created this falkerson application for you.
+
+This file contains a summary of what you will find in each of the files that
+make up your falkerson application.
+
+
+falkerson.vcxproj
+    This is the main project file for VC++ projects generated using an Application Wizard.
+    It contains information about the version of Visual C++ that generated the file, and
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+falkerson.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard. 
+    It contains information about the association between the files in your project 
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+falkerson.cpp
+    This is the main application source file.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named falkerson.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
+Other notes:
+
+AppWizard uses "TODO:" comments to indicate parts of the source code you
+should add to or customize.
+
+/////////////////////////////////////////////////////////////////////////////
---
+++ falkerson/falkerson/falkerson.cpp
@@ -0,0 +1,129 @@
+// falkerson.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+const int MAX_VERTICES = 40;
+const int MY_INFINITY = 10000;
+ 
+vector<vector<int>> currentFlow, arcCapacity;
+int vertCount, maxFlow = 0;
+vector<int> flow;
+vector<int> link;
+vector<int> queue;
+int queuePtr, queueCount; 
+
+int FindPath(int source, int target) 
+{
+	queuePtr = 0; queueCount = 1; queue[0] = source;
+	link[target] = -1; 
+	int i;
+	int CurVertex;
+	flow.assign(vertCount, 0);
+	flow[source] = MY_INFINITY;
+	while (link[target] == -1 && queuePtr < queueCount)
+	{
+		CurVertex = queue[queuePtr];
+		for (i = 0; i < vertCount; i++)
+			if ((arcCapacity[CurVertex][i] - currentFlow[CurVertex][i]) > 0 && flow[i] == 0)
+			{
+				queue[queueCount] = i; queueCount++;
+				link[i] = CurVertex; 
+									 
+				if (arcCapacity[CurVertex][i] - currentFlow[CurVertex][i] < flow[CurVertex])
+					flow[i] = arcCapacity[CurVertex][i];
+				else
+					flow[i] = flow[CurVertex];
+			}
+		queuePtr++;
+	}
+	if (link[target] == -1) return 0; 
+									  
+	CurVertex = target;
+	while (CurVertex != source) 
+	{
+		currentFlow[link[CurVertex]][CurVertex] += flow[target];
+		CurVertex = link[CurVertex];
+	}
+	return flow[target]; 
+}
+
+void MaxFlow(int source, int target)
+{
+	int AddFlow;
+	do
+	{
+		AddFlow = FindPath(source, target);
+		maxFlow += AddFlow;
+	} 
+	while (AddFlow >0);
+}
+
+void readGraph(ifstream & is)
+{
+	int n = 0;
+	string line;
+	while (getline(is, line))
+	{
+		arcCapacity.push_back({});
+		currentFlow.push_back({});
+		stringstream ss(line);
+		for (;;)
+		{
+			int node;
+			ss >> node;
+			if (!ss) break;
+			arcCapacity[n].push_back(node);
+			currentFlow[n].push_back(0);
+		}
+		n++;
+	}
+}
+
+void writeResult()
+{
+	ofstream out;
+	out.open("output.txt");
+
+	out << "Max flow: " << maxFlow << endl;
+
+	out.close();
+}
+
+int main(int argc, char* argv[])
+{
+	try
+	{
+
+		if (argc != 2)
+		{
+			throw exception("Usage: falkeson.exe <input-file>");
+		}
+
+		ifstream fin(argv[1]);
+		if (!fin.is_open())
+		{
+			throw exception("Cannot open file.");
+		}
+
+		readGraph(fin);
+
+		vertCount = arcCapacity.size();
+
+		queue.assign(vertCount, 0);
+		link.assign(vertCount, 0);
+
+		MaxFlow(0, vertCount - 1);
+
+		writeResult();
+	}
+	catch (exception & e)
+	{
+		cout << e.what() << endl;
+		return 1;
+	}
+
+	return 0;
+}
